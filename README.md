02936a3572a34fc8fc2aae553e9f7f33595232b6
juristr <juri.strumpflohner@gmail.com>
2015-07-25 09:06:13 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,30 @@
+angular-translate Pluggable Loader
+---
+
+Pluggable Loader is a custom loader for [angular-translate](https://github.com/angular-translate/angular-translate). It aims to facilitate the modularization of the applications s.t. each module can contribute their own translations and possibly be independent in the kind of angular-translate loader it uses.
+
+- where it started: https://github.com/angular-translate/angular-translate/issues/1125
+- **First proof of concept demo: ** http://plnkr.co/edit/ygaMvPBxYN3VWXEesWto?p=preview
+
+> **Note:** I'm about to set this up, so it could be currently a bit unstable. Come back in a couple of days as I'll have it setup completely.
+
+## Installation
+
+(soon)
+
+## Development
+
+```
+$ npm install -g gulp
+$ npm install
+```
+
+### Build
+
+```
+$ gulp build
+```
+
+## Contribute
+
+(soon)
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var cache = require('gulp-cached');
+var plumber = require('gulp-plumber');
+var babel = require('gulp-babel');
+var ngAnnotate = require('gulp-ng-annotate');
+var vinylPaths = require('vinyl-paths');
+var del = require('del');
+var changed = require('gulp-changed');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var lessPluginCleanCSS = require('less-plugin-clean-css');
+var cleancss = new lessPluginCleanCSS({advanced: true});
+var htmlMin = require('gulp-minify-html');
+var uglify = require('gulp-uglify');
+
+var path = {
+    src: 'src/**/*.js',
+    output: 'dist/'
+};
+
+var compilerOptions = {
+  modules: 'umd',
+  // moduleIds: false,
+  // externalHelpers: true,
+  comments: true,
+  compact: false,
+};
+
+gulp.task('clean', function() {
+  return gulp.src([path.output])
+    .pipe(vinylPaths(del));
+});
+
+gulp.task('compile', function(){
+    return gulp.src(path.src)
+        .pipe(cache('es6'))
+        .pipe(plumber())
+        .pipe(changed(path.output, { extension: '.js' }))
+        .pipe(sourcemaps.init())
+        .pipe(babel(compilerOptions))
+        .pipe(ngAnnotate({
+          sourceMap: true,
+          gulpWarnings: false
+        }))
+        .pipe(uglify())
+        .pipe(concat('angular-translate-loader-pluggable.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(path.output));
+});
+
+gulp.task('build', ['clean', 'compile']);
+
+gulp.task('default', []);
